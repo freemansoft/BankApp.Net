891eca936e3149ee18aaf48243ccdfe6f8bf9efd
Joe Freeman <joe+github@freemansoft.com>
2017-07-07 00:01:59 -0400

    Added Selenium Tests Examples.  Removed Xunit

---
+++ Selenium.Test/WebSearchExampleSteps.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using TechTalk.SpecFlow;
+
+namespace Selenium.Test
+{
+    [Binding]
+    public class WebSearchExampleSteps
+    {
+        // For additional details on SpecFlow step definitions see http://go.specflow.org/doc-stepdef
+
+        [BeforeScenario]
+        public void BeforeScenario()
+        {
+            // IWebDriver driver = new ChromeDriver();
+            IWebDriver driver = new ChromeDriver();
+
+            // you protection mode must be the same for all zones and you need to be zoomed at 100% to use IE
+            //IWebDriver driver = new InternetExplorerDriver();
+            //IWebDriver driver = new InternetExplorerDriver("C:\\class\\SeleniumSearch - Partial Solution\\vs2010solution\\packages\\Selenium.WebDriver.IEDriver.2.46.0.0\\driver");
+            ScenarioContext.Current.Set<IWebDriver>(driver, "driver");
+        }
+
+        [AfterScenario]
+        public void AfterScenario()
+        {
+            IWebDriver driver = ScenarioContext.Current.Get<IWebDriver>("driver");
+            driver.Quit();
+        }
+
+        [Given(@"I want to search with ""(.*)""")]
+        public void GivenIWantToSearchWithGoogle(string p0)
+        {
+            IWebDriver driver = ScenarioContext.Current.Get<IWebDriver>("driver");
+            driver.Navigate().GoToUrl("http://www." + p0 + ".com");
+        }
+
+
+        /// <summary>
+        /// Saves the last search term to be used in "Then" statements
+        /// </summary>
+        /// <param name="p0"></param>
+        [When(@"When I search for ""(.*)""")]
+        public void WhenWhenISearchFor(string p0)
+        {
+            ScenarioContext.Current.Set<string>(p0, "lastSearch");
+            IWebDriver driver = ScenarioContext.Current.Get<IWebDriver>("driver");
+            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            wait.Until(d => d.FindElement(By.Name("q")));
+            IWebElement queryField = driver.FindElement(By.Name("q"));
+            queryField.SendKeys(p0);
+            queryField.Submit();
+            // wait until we get some link with FreemanSoft in it
+            Assert.IsNotNull(wait.Until(d => d.FindElement(By.PartialLinkText(p0))));
+        }
+
+        /// <summary>
+        /// Assumes "search term " refers to last search term
+        /// </summary>
+        [Then(@"My search term should be in the title bar")]
+        public void MySearchTermShouldBeInTheTitleBar()
+        {
+            string lastSearch = ScenarioContext.Current.Get<string>("lastSearch");
+            IWebDriver driver = ScenarioContext.Current.Get<IWebDriver>("driver");
+            string title = driver.Title;
+            StringAssert.Contains(title, lastSearch);
+        }
+    }
+
+}
---
+++ Selenium.Test/WebSearchExampleTest.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Support.UI;
+
+namespace Selenium.Test
+{
+    [TestClass]
+    public class WebSearchExampleTest
+    {
+        public WebSearchExampleTest()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        private IWebDriver driver;
+        private WebDriverWait wait;
+
+        //
+        // Use TestInitialize to run code before running each test 
+        [TestInitialize()]
+        public void MyTestInitialize()
+        {
+            //driver = new ChromeDriver();
+            // https://code.google.com/p/selenium/wiki/InternetExplorerDriver#Required_Configuration
+            var options = new InternetExplorerOptions
+            {
+                IgnoreZoomLevel = true
+            };
+            //driver = new InternetExplorerDriver(options);
+            //driver = new InternetExplorerDriver();
+            //driver = new ChromeDriver();
+            driver = new ChromeDriver();
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+        }
+        //
+        // Use TestCleanup to run code after each test has run
+        [TestCleanup()]
+        public void MyTestCleanup()
+        {
+            driver.Quit();
+        }
+        //
+
+        [TestMethod]
+        public void Example_SearchForFreemanSoft()
+        {
+            string desiredLinkText = "FreemanSoft Inc";
+            driver.Navigate().GoToUrl("http://www.google.com");
+            IWebElement queryField = driver.FindElement(By.Name("q"));
+            queryField.SendKeys("freemansoft");
+            queryField.Submit();
+            // wait until a link for the search term shows up before doing anything else
+            Assert.IsNotNull(wait.Until(d => d.FindElement(By.LinkText(desiredLinkText))));
+            // google puts search term in title bar after search
+            StringAssert.Contains(driver.Title, "freemansoft");
+            // find the link again and click on it to go to the home page
+            IWebElement link = wait.Until(d => d.FindElement(By.LinkText(desiredLinkText)));
+            link.Click();
+            StringAssert.Contains(driver.Title, "FreemanSoft Inc");
+        }
+
+        [TestMethod]
+        public void Example_SearchForAmazon()
+        {
+            string desiredLinkText = "Amazon.com® Official Site";
+            driver.Navigate().GoToUrl("http://www.google.com");
+            IWebElement queryField = driver.FindElement(By.Name("q"));
+            queryField.SendKeys("amazon");
+            queryField.Submit();
+            // wait until the field we are interested in shows up - otherwise fail test
+            Assert.IsNotNull(wait.Until(d => d.FindElement(By.PartialLinkText(desiredLinkText))));
+            // now actually grab the link to click
+            IWebElement link = driver.FindElement(By.PartialLinkText(desiredLinkText));
+            link.Click();
+            Thread.Sleep(5000);
+
+        }
+    }
+}
