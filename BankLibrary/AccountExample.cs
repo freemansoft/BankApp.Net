cdc0c97b533a01fef58abf15352043bbd3a3d4a2
Joe Freeman <joe@freemansoft.com>
2017-07-11 11:44:20 -0400

    Add simple account Example

---
+++ BankLibrary/AccountExample.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankLibrary
+{
+    public class AccountExample
+    {
+        private string firstName;
+
+        public string GetFirstName()
+        {
+            return firstName;
+        }
+
+        public void SetFirstName(string value)
+        {
+            if (value == null)
+                {
+                throw new ArgumentException("first name cannot be null");
+                }
+            firstName = value;
+        }
+
+        private string lastName;
+
+        public string GetLastName()
+        {
+            return lastName;
+        }
+
+        public void SetLastName(string value)
+        {
+            if (value == null)
+            {
+                throw new ArgumentException("last name cannot be null");
+            }
+            lastName = value;
+        }
+
+        public string AccountNumber { get; set; }
+
+        public Decimal AccountBalance { get; set; }
+
+        public void setupAccount()
+        {
+            AccountNumber = Guid.NewGuid().ToString();
+            AccountBalance = Decimal.Zero;
+        }
+
+        public object getAccountNumber()
+        {
+            // should we throw an exception if an account doesn't exist?
+            return AccountNumber;
+        }
+
+        public decimal getBalance()
+        {
+            return AccountBalance;
+        }
+    }
+}
