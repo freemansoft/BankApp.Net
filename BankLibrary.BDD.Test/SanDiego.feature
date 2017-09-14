acaa3a4c7a927806acc6af73090eb9a7199e48ec
Joe Freeman <joe@freemansoft.com>
2017-09-13 21:12:59 -0400

    add classes developed in San Diego.  We will delete later but still have in history

---
+++ BankLibrary.BDD.Test/SanDiego.feature
@@ -0,0 +1,33 @@
+﻿Feature: SanDiego
+    In order to manipulate an account
+
+@mytag
+Scenario: Deposit money and verify balance
+    Given I have an account with "20" dollars in it
+    When I deposit "100" dollars 
+    Then My balance should be "120" dollars
+
+Scenario: Withdraw money and verify balance
+    Given I have an account with "220" dollars in it
+    When I withdraw "100" dollars 
+    Then My balance should be "120" dollars
+
+Scenario Outline: Deposit and withdraw verify balance
+    Given I have an account with <initial> dollars in it
+    When I deposit <deposit_amount> dollars 
+    When I withdraw <withdraw_amount> dollars 
+    Then My balance should be <final_balance> dollars
+
+Examples:
+    | initial | deposit_amount | withdraw_amount | final_balance |
+    | "100"    | "20"      | "10"       | "110"     |
+    | "200"    | "0"       | "10"       | "190"    |
+    | "10"      | "8"       | "6"     |  "12"   |
+
+
+Scenario: Single deposit and withdraw money and verify balance
+    Given I have an account with "220" dollars in it
+    When I deposit "100" dollars 
+    When I withdraw "100" dollars 
+    Then My balance should be "220" dollars
+
---
+++ BankLibrary.Test/SimpleAccountTest.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace BankLibrary.Test
+{
+    [TestClass]
+    public class SimpleAccountTest
+    {
+        [TestMethod]
+        public void Simple_NewAccountBalance()
+        {
+            SimpleAccount objectUnderTest = new BankLibrary.SimpleAccount("Bill", "Gates", Decimal.Zero);
+            Assert.AreEqual(Decimal.Zero, objectUnderTest.getCurrentBalance());
+
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Simple_NewAccountNoFirstName()
+        {
+            SimpleAccount objectUnderTest = new BankLibrary.SimpleAccount(null, "Gates", Decimal.Zero);
+
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Simple_NewAccountNoLastName()
+        {
+            SimpleAccount objectUnderTest = new BankLibrary.SimpleAccount("Bill", null, Decimal.Zero);
+
+        }
+
+        [TestMethod]
+        public void Simple_Withdrawl()
+        {
+            SimpleAccount objectUnderTest = new BankLibrary.SimpleAccount("Bill", "Gates", new decimal(200.0));
+            objectUnderTest.Debit(new decimal(20.0));
+            Assert.AreEqual(new decimal(180.0), objectUnderTest.getCurrentBalance());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Simple_WithdrawlOverdraft()
+        {
+            SimpleAccount objectUnderTest = new BankLibrary.SimpleAccount("Bill", "Gates", new decimal(200.0));
+            objectUnderTest.Debit(new decimal(220.0));
+        }
+
+        [TestMethod]
+        public void Simple_WithdrawlOverdraftWithEmail()
+        {
+            SimpleAccount objectUnderTest = new BankLibrary.SimpleAccount("Bill", "Gates", new decimal(200.0));
+            Mock<IEmailSender> myMock = new Mock<IEmailSender>();
+            myMock.Setup(c => c.SendEmail(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()))
+                .Returns(true);
+            // inject the email sender via setter injection.  mock.Object returns the actual mock
+            objectUnderTest.sender = myMock.Object;
+            try
+            {
+                objectUnderTest.Debit(new decimal(220.0));
+                // don't need to verify this because it id done above.  this test should only verify one behavior
+                //Assert.Fail("should have sent email and then thrown an exception");
+            } catch (ArgumentException e)
+            {
+                Console.WriteLine("overdraft attempt causes email and then exception so this is good "+e);
+            }
+            // we ate the exception so go back and verify that the mock was invoked to send email
+            myMock.Verify(
+                m => m.SendEmail(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()), 
+                Times.Once);
+       }
+
+    }
+}
